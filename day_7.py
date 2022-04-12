e9045f9e6128372ae47ce5d2fd5829921931f9c5
Ajai Olufisayo <tadeajai4@gmail.com>
2022-04-12 19:22:30 +0100

    Added instructions to the wordle game for day 7

--- day_7.py
+++ day_7.py
@@ -6,40 +6,49 @@ def play_wordle():
     """
     A simple python wordle game
     """
-    secret_word = getline('hardcoded_word_list.txt', randint(0, 50)).strip()  # get a random word from the word-file
-    round_num = 1  # rounds played
-    guess = input(
-        'WORDLE \n \nEnter your first guess: ').lower().strip()  # get and sanitise the string for the first guess
-    while round_num <= 6:
-        if round_num != 1:
-            guess = input(
-                f"Enter guess {round_num}: ").lower().strip()  # get and sanitise the string for the subsequent guesses
-        while len(
-                guess) != 5 or not guess.isalpha():  # ensure the guess contains only alphabets and has 5 characters
-            print("Only a word of 5 letters is allowed!!, Let's try that again.")
-            guess = input(f'Enter guessed {round_num}: ')
-        feedback = ['x', 'x', 'x', 'x', 'x']  # feedback to display at the end of every guess
-        guess_copy = guess  # make copies of the guess and the secret word
-        secret_word_copy = secret_word
-        for index, letter in enumerate(guess_copy):  # checks if any letter is in the right position
-            if secret_word_copy[index] == letter:
-                feedback[index] = '√'
-                guess_copy = guess_copy[:index] + '1' + guess_copy[
-                                                        index + 1:]  # if it is, replace it with 0 and 1 in both copies
-                secret_word_copy = secret_word_copy[:index] + '0' + secret_word_copy[index + 1:]
-        if feedback.count('√') == 5:  # check if the player guessed the word successfully
-            print(f"Congrats. You guess the word after {round_num} attempt/s")
-            break
-        for index, letter in enumerate(guess):  # check if the letter is in what is left
-            if letter in secret_word_copy:
-                feedback[index] = '+'
-                guess_copy = guess_copy[:index] + '1' + guess_copy[index + 1:]
-                secret_word_copy.replace(letter, '0', 1)
-        print(f"your feedback is {''.join(feedback)}")
-        round_num += 1  # increment the round
-        print(f"ROUND {round_num}")
-    print(f'GAME OVER\nThe word was {secret_word}')
 
+
+greetings = '''WORDLE\nINSTRUCTIONS\n# Description: Wordle is a single player game, in which a user is required to guess a 5-letter hidden word in 6 Attempts.
+- The user makes a first guess..
+- Then you get a feedback looking like "√××√+".
+ * "√" Indicates that the letter is in the secret word and in the correct position.
+ * "+" indicates that the letter is in the hidden word but in a different position.
+ * "×" indicates that the letter is not in the word
+- The player is given a total of 6 guesses in which he must guess the secret word.'''
+print(greetings)
+secret_word = getline('hardcoded_word_list.txt', randint(0, 50)).strip()  # get a random word from the word-file
+round_num = 1  # rounds played
+guess = input(
+    'Enter your first guess: ').lower().strip()  # get and sanitise the string for the first guess
+while round_num <= 6:
+    if round_num != 1:
+        guess = input(
+            f"Enter guess {round_num}: ").lower().strip()  # get and sanitise the string for the subsequent guesses
+    while len(
+            guess) != 5 or not guess.isalpha():  # ensure the guess contains only alphabets and has 5 characters
+        print("Only a word of 5 letters is allowed!!, Let's try that again.")
+        guess = input(f'Enter guess {round_num}: ')
+    feedback = ['x', 'x', 'x', 'x', 'x']  # feedback to display at the end of every guess
+    guess_copy = guess  # make copies of the guess and the secret word
+    secret_word_copy = secret_word
+    for index, letter in enumerate(guess_copy):  # checks if any letter is in the right position
+        if secret_word_copy[index] == letter:
+            feedback[index] = '√'
+            guess_copy = guess_copy[:index] + '1' + guess_copy[
+                                                    index + 1:]  # if it is, replace it with 0 and 1 in both copies
+            secret_word_copy = secret_word_copy[:index] + '0' + secret_word_copy[index + 1:]
+    if feedback.count('√') == 5:  # check if the player guessed the word successfully
+        print(f"Congrats. You guessed the word after {round_num} attempt/s")
+        break
+    for index, letter in enumerate(guess):  # check if the letter is in what is left
+        if letter in secret_word_copy:
+            feedback[index] = '+'
+            guess_copy = guess_copy[:index] + '1' + guess_copy[index + 1:]
+            secret_word_copy.replace(letter, '0', 1)
+    print(f"your feedback is {''.join(feedback)}")
+    round_num += 1  # increment the round
+    print(f"ROUND {round_num}")
+print(f'GAME OVER\nThe word was {secret_word}')
 
 if __name__ == '__main__':
     play_again = True
