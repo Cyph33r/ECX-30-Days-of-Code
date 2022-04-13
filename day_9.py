c2b447898f0db65b7f2714212b3f83e0521c1f2f
Ajai Olufisayo <tadeajai4@gmail.com>
2022-04-13 13:22:47 +0100

    Added day 1 file made slight changes to day 9

--- day_9.py
+++ day_9.py
@@ -1,18 +1,18 @@
 def _is_prime(num: int) -> bool:
     """
-    Determines if a number is a prime number or not i.e it only whole divisors are 1 and itself
+    Determines if a number is a prime number or not i.e. it only whole divisors are 1 and itself
     :param num:the number to be determined
     :returns: True if the :num is a prime number False otherwise
     """
     assert isinstance(num, int), f'Expected a class int for num but a {num.__class__} was passed'
-    if num <= 1:  # return false if the parameter is 1 or less since they are not prime numbers
+    if num <= 1:  # return False if the parameter is 1 or less since they are not prime numbers
         return False
     is_prime = True
-    for i in range(2, num // 2 + 1):  # start half-way since 2 is the lowest divisor
+    for i in range(2, num // 2 + 1):  # start half-way since 2 is the lowest possible divisor
         if num % i == 0:  # check if the number is divisible
             is_prime = False  # if some is_prime becomes False and is returned
             break
-    return is_prime
+    return is_prime # if the for loop completed successfully this returns True
 
 
 if __name__ == '__main__':
